2ac977fcecb0d3ea75ba395e3ccc20f1851ef98f
LittleZocki98 <littlezocki98@gmail.com>
2017-07-08 11:40:38 +0200

    Removed unnecessary usings in Vector3D.cs and filled the HexaLeg.cs class with content

--- Diplomarbeit/HexaLeg.cs
+++ Diplomarbeit/HexaLeg.cs
@@ -1,13 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Diplomarbeit {
   class HexaLeg {
-    public HexaLeg() {
 
+    private Vector3D offset, thigh, shank;
+    private double alpha, beta, gamma;
+
+    public double Alpha {
+      get { return this.alpha; }
+      set { this.alpha = value; }
+    }
+    public double Beta {
+      get { return this.beta; }
+      set { this.beta = value; }
+    }
+    public double Gamma {
+      get { return this.gamma; }
+      set { this.gamma = value; }
+    }
+
+    public HexaLeg(Vector3D Offset, Vector3D Thigh, Vector3D Shank) {
+      this.alpha = 0.0;
+      this.beta = 0.0;
+      this.gamma = 0.0;
+    }
+
+    public void CalculateAngles(Vector3D Point) {
+      double a = Math.Sqrt(Point.X * Point.X + Point.Y * Point.Y);
+      double b = (Point.Z - this.offset.Z) * (Point.Z - this.offset.Z);
+      double c = (a - this.offset.X) * (a - this.offset.X);
+
+      this.alpha = Math.Atan2(Point.Y, Point.X) - Math.Asin((this.offset.Y + this.thigh.Y) / a);
+      this.gamma = Math.Acos(((b + c) - (this.thigh.Z * this.thigh.Z) - (this.shank.Z * this.shank.Z)) / (2.0 * this.thigh.Z * this.shank.Z));
+      this.beta = (Math.PI / 2.0) - (Math.Atan2(Point.Z - this.offset.Z, a - this.offset.X) + Math.Asin(Math.Sin(gamma) * (this.shank.Z / Math.Sqrt(b + c))));
     }
   }
 }
--- Diplomarbeit/Vector3D.cs
+++ Diplomarbeit/Vector3D.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Diplomarbeit {
+﻿namespace Diplomarbeit {
   class Vector3D {
     private double x, y, z;
 
